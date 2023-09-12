5eae67c3f53a70383457c83307931dbeb3398fa0
Allexus Technology <ale988240769@gmail.com>
2023-09-12 17:07:03 -0300

    testando dois servidores em um na vercel x7

--- server/index.js
+++ server/index.js
@@ -1,6 +1,6 @@
 const app = require('express')()
 const server = require('http').createServer(app)
-const io = require('socket.io')(server, {cors: {origin: 'https://matchat.vercel.app'}})
+const io = require('socket.io')(server, {cors: {origin: '*'}})
 
 const PORT = 9001
 
